608e2de84f0bb94b011419b8252fcaf730ec1424
neosu <neo@tcodestudio.com>
2023-11-20 20:16:56 +0800

    update linux build script, disable the pulse_audio

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -21,7 +21,26 @@ cp $GFILE $WEBRTC_SRC/.gclient
 export DEPOT_TOOLS_WIN_TOOLCHAIN=0
 export ARCH=x64
 cd $WEBRTC_SRC/src
-gn gen out/linux-$ARCH --args="target_os=\"linux\" target_cpu=\"$ARCH\" is_debug=false rtc_enable_google_benchmarks=false rtc_include_tests=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" is_component_build=false use_rtti=true use_custom_libcxx=false rtc_enable_protobuf=false rtc_include_tests=false rtc_build_examples=false libwebrtc_desktop_capture=true use_cxx17=true" 
+
+# gn gen out/linux-$ARCH --args="target_os=\"linux\" target_cpu=\"$ARCH\" is_debug=false rtc_enable_google_benchmarks=false rtc_include_tests=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" is_component_build=false use_rtti=true use_custom_libcxx=false rtc_enable_protobuf=false rtc_include_tests=false rtc_build_examples=false libwebrtc_desktop_capture=true use_cxx17=true" 
+
+# disbale audio
+gn gen out/linux-$ARCH --args="target_os=\"linux\" target_cpu=\"$ARCH\" \
+is_debug=false \
+rtc_enable_google_benchmarks=false \
+rtc_include_tests=false \
+rtc_use_h264=true \
+ffmpeg_branding=\"Chrome\" \
+is_component_build=false \
+use_rtti=true use_custom_libcxx=false \
+rtc_enable_protobuf=false \
+rtc_include_tests=false \
+rtc_build_examples=false \
+libwebrtc_desktop_capture=true \
+rtc_exclude_audio_processing_module=true \
+rtc_include_pulse_audio=false \
+use_cxx17=true" 
+
 ninja -C out/linux-$ARCH
 
 cd $ROOT
